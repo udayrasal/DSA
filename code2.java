060b492e1226ac85523adb5738d4c8688a33a9b7
udayrasal <flutterudayrasal@gmail.com>
2024-01-15 22:11:17 +0530

    day1 code2

---
+++ code2.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        Arrays.sort(arr);
+        int s=0;
+        int e=n-1;
+        while(s<e){
+            if(arr[s]+arr[e]==x)
+                return true;
+            else if(arr[s]+arr[e]<x){
+                s++;
+            }
+            else{
+                e--;
+            }
+        }
+        return false;
+        // code here
+    }
+}
